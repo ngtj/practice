9bbbd58325fa6307258491c1fdf6e0d53091bad9
Jun Nagata <ngtj1983@gmail.com>
2012-05-04 01:32:21 +0900

    add ignore file

--- thread/source/main.cpp
+++ thread/source/main.cpp
@@ -2,7 +2,35 @@
  * @brief サンプルエントリ
  */
 #include <cstdio>
+#include <cstdlib>
+#include <pthread.h>
+
+static void *func(void *args) {
+	for (int i = 0; i < 10; i++) {
+		puts(" b");
+		sleep(1);
+	}
+
+	return NULL;
+}
 
 int main (void) {
-	printf("Hello Thread\n");	
+	pthread_t thread;
+
+	// スレッド生成
+	if (pthread_create(&thread, NULL, func, NULL) != 0) {
+		return EXIT_FAILURE;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		puts("a");
+		sleep(1);
+	}
+
+	// スレッド待機
+	if (pthread_join(thread, NULL) != 0) {
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
